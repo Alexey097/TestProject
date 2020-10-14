b48341384a521a96e4652616936123cc5f1ddbc5
Alexey097 <alex_su16@mail.ru>
2020-10-14 21:06:06 +0300

    TEst

--- src/mypackage/Test.java
+++ src/mypackage/Test.java
@@ -5,5 +5,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.print("hi ");
         System.out.println("its my hundred programm");
+        System.out.println("Hello github");
     }
 }
