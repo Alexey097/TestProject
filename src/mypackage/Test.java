e3348648a7addcd3b6a098d13cdb3e3e3ff1dc8e
Alexey097 <alex_su16@mail.ru>
2020-10-14 20:56:49 +0300

    Initial commit

---
+++ src/mypackage/Test.java
@@ -0,0 +1,9 @@
+package mypackage;
+
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.print("hi ");
+        System.out.println("its my hundred programm");
+    }
+}
